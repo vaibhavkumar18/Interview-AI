4763b4eb7877a02c38b30ca25154cc66e6ea5d5b
Vaibhav <vaibhavkumarnigam30@gmail.com>
2026-05-11 21:35:43 +0530

    Resolve Interview API

--- src/features/Interview/services/interview.api.js
+++ src/features/Interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BaseURL = import.meta.env.VITE_BACKEND_URL;
 const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: BaseURL,
     withCredentials: true,
 });
 
